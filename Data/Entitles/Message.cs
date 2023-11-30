51c16d94d1a8393bb7849cf4cb3c629826215e54
bcluc <21521898@gm.uit.edu.vn>
2023-11-30 13:43:17 +0700

    Add project files.

---
+++ Data/Entitles/Message.cs
@@ -0,0 +1,29 @@
+﻿namespace Chatable.Data.Entitles
+{
+    public class Message
+    {
+        public string MessageId { get; set; }
+        public string SenderId { get; set; }
+        public string ConversationId { get; set; }
+        public DateTime SendAt { get; set; }
+        public string MessageType { get; set; }
+        public string TextContent { get; set; }
+        public string ImageContent { get; set; }
+        public string AudioContent { get; set; }
+        public string VideoContent { get; set; }
+        public Message() { }
+
+        public Message(string messageId, string senderId, string conversationId, DateTime sendAt, string messageType, string textContent, string imageContent, string audioContent, string videoContent)
+        {
+            MessageId = messageId;
+            SenderId = senderId;
+            ConversationId = conversationId;
+            SendAt = sendAt;
+            MessageType = messageType;
+            TextContent = textContent;
+            ImageContent = imageContent;
+            AudioContent = audioContent;
+            VideoContent = videoContent;
+        }
+    }
+}
---
+++ Data/Entitles/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Chatable.Data.Entitles
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Avatar { get; set; }
+        public string LastTimeOnline { get; set; }
+        public User() { }
+        public User(string userId, string userName, string fullName, string avatar, string lastTimeOnline)
+        {
+            UserId = userId;
+            UserName = userName;
+            FullName = fullName;
+            Avatar = avatar;
+            LastTimeOnline = lastTimeOnline;
+        }
+    }
+}
