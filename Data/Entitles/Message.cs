d4f8bf4040e1674621f2a0593c301a8da3c378e4
bcluc <21521898@gm.uit.edu.vn>
2023-12-04 11:48:28 +0700

    chat list (not complete UI)

--- Data/Entitles/Message.cs
+++ Data/Entitles/Message.cs
@@ -7,23 +7,18 @@
         public string ConversationId { get; set; }
         public DateTime SendAt { get; set; }
         public string MessageType { get; set; }
-        public string TextContent { get; set; }
-        public string ImageContent { get; set; }
-        public string AudioContent { get; set; }
-        public string VideoContent { get; set; }
+        
+        public string Content { get; set; }
         public Message() { }
 
-        public Message(string messageId, string senderId, string conversationId, DateTime sendAt, string messageType, string textContent, string imageContent, string audioContent, string videoContent)
+        public Message(string messageId, string senderId, string conversationId, DateTime sendAt, string messageType, string content)
         {
             MessageId = messageId;
             SenderId = senderId;
             ConversationId = conversationId;
             SendAt = sendAt;
             MessageType = messageType;
-            TextContent = textContent;
-            ImageContent = imageContent;
-            AudioContent = audioContent;
-            VideoContent = videoContent;
+            Content = content;
         }
     }
 }
--- Data/Entitles/User.cs
+++ Data/Entitles/User.cs
@@ -16,5 +16,6 @@
             Avatar = avatar;
             LastTimeOnline = lastTimeOnline;
         }
+        public bool IsSelected { get; set; }
     }
 }
