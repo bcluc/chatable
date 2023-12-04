dba168e32e099853d50eb5078a590435be91012c
Nhongnhong-0101 <21522423@gm.uit.edu.vn>
2023-12-04 17:58:43 +0700

    UI tạm login

--- Data/Entitles/User.cs
+++ Data/Entitles/User.cs
@@ -4,14 +4,16 @@
     {
         public string UserId { get; set; }
         public string UserName { get; set; }
+        public string Password { get; set; }    
         public string FullName { get; set; }
         public string Avatar { get; set; }
         public string LastTimeOnline { get; set; }
         public User() { }
-        public User(string userId, string userName, string fullName, string avatar, string lastTimeOnline)
+        public User(string userId, string userName, string password, string fullName, string avatar, string lastTimeOnline)
         {
             UserId = userId;
             UserName = userName;
+            Password = password;
             FullName = fullName;
             Avatar = avatar;
             LastTimeOnline = lastTimeOnline;
