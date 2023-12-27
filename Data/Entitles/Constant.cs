05a70cc2564b27feb5d1a894ef338326c3dc82bf
Nhongnhong-0101 <21522423@gm.uit.edu.vn>
2023-12-27 22:55:13 +0700

    lỗi UI profile dialog

--- Data/Entitles/Constant.cs
+++ Data/Entitles/Constant.cs
@@ -27,24 +27,26 @@ namespace Chatable.Data.Entitles
 
         public static readonly List<Conversation> chats  = new List<Conversation>
         {
-            new User("nhubaole", "Trần Lê Hoàng Lâm","Nam","2023-11-19 07:58:48.084+00","2023-11-19 07:58:48.084+00","BC_nhubaole"),
-            new User("nhihuynh", "Công chúa Elsa","Nữ", "2023-12-04 17:03:22.2+00","2023-12-04 17:03:22.2+00", "BC_nhihuynh"),
+            new User("nhubaole", "Trần Lê Hoàng Lâm","","Nam","2023-11-19 07:58:48.084+00","2023-11-19 07:58:48.084+00","BC_nhubaole"),
+            new User("nhihuynh", "Công chúa Elsa","","Nữ", "2023-12-04 17:03:22.2+00","2023-12-04 17:03:22.2+00", "BC_nhihuynh"),
             new Group("123","Group Local Test", "", "BC", "2023-12-04 17:03:22.2+00")
         };
 
         public static readonly IList<User> usersList = new List<User>
         {
-        new User("nhubaole", "Trần Lê Hoàng Lâm","Name","2023-11-19 07:58:48.084+00","2023-11-19 07:58:48.084+00", "BC_nhubaole"),
-        new User("nhihuynh", "Công chúa Elsa","Nữ", "2023-12-04 17:03:22.2+00","2023-12-04 17:03:22.2+00", "BC_nhihuynh"),
+        new User("nhubaole", "Trần Lê Hoàng Lâm","", "Name","2023-11-19 07:58:48.084+00","2023-11-19 07:58:48.084+00", "BC_nhubaole"),
+        new User("nhihuynh", "Công chúa Elsa","", "Nữ", "2023-12-04 17:03:22.2+00","2023-12-04 17:03:22.2+00", "BC_nhihuynh"),
         };
 
-        public static readonly User currentUser = new("Bùi Cường", "Bùi Cường", "Nam", "2023-12-05 11:27:29.397+00", "2023-12-05 11:27:29.397+00","BC");
+        public static readonly User currentUser = new("Bùi Cường", "Bùi Cường", "", "Nam", "2023-12-05 11:27:29.397+00", "2023-12-05 11:27:29.397+00","BC");
         
         public static readonly Message visibleMessage = new("0", "BC", "1", DateTime.Now, "Text", "Say hi");
 
         public static readonly Group currentGroup = new Group("123", "Group Local Test", "", "BC", "2023-12-04 17:03:22.2+00");
 
-        public static readonly User oppositeUser = new User("nhubaole", "Trần Lê Hoàng Lâm", "Nam", "2023-11-19 07:58:48.084+00", "2023-11-19 07:58:48.084+00", "BC_nhubaole");
+        public static readonly User oppositeUser = new User("nhubaole", "Trần Lê Hoàng Lâm", "", "Nam", "2023-11-19 07:58:48.084+00", "2023-11-19 07:58:48.084+00", "BC_nhubaole");
+       
+
     }
 
     
