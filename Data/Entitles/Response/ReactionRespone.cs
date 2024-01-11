05bbe9f2ef250296f668c6c2c65fb4c744d0c858
petrichor-hl <tl.hoanglam174@gmail.com>
2024-01-12 00:41:11 +0700

    fix

--- Data/Entitles/Response/ReactionRespone.cs
+++ Data/Entitles/Response/ReactionRespone.cs
@@ -6,5 +6,7 @@
         public string SenderId { get; set; }
         public string MessageId { get; set; }
         public int Type { get; set; }
+        public string SenderName { get; set; }
+        public string SenderAvatar { get; set; }
     }
 }
