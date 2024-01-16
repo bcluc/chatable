56f32961b31131b7157a0d98985b3e701c2b9818
bcluc <21521898@gm.uit.edu.vn>
2024-01-16 13:24:42 +0700

    update

---
+++ AppTheme.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+
+namespace Chatable
+{
+    public class AppTheme
+    {
+        private readonly IJSRuntime js;
+
+        public AppTheme(IJSRuntime js)
+        {
+            this.js = js;
+        }
+
+        private bool isLeftMode = false;
+
+        public bool IsLeftMode
+        {
+            get => isLeftMode;
+            set
+            {
+                isLeftMode = value;
+                js.InvokeVoidAsync("setLeftMode", value);
+            }
+        }
+    }
+}
